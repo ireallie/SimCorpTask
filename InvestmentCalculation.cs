3d379a457ab868458251b8990e201b9f32fbbe77
ireallie <338godofwar@gmail.com>
2022-05-22 22:02:02 +0200

    Add project files.

---
+++ InvestmentCalculation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimCorpTask
+{
+    public static class InvestmentCalculation
+    {
+        /// <summary>
+        /// A function that calculates mountly payment that doesn't change each month and represents the sum of two components: part of initial principal and interest amount.
+        /// </summary>
+        /// <param name="investmentDuration"></param>
+        /// <param name="investmentSum"></param>
+        /// <param name="interestRate"></param>
+        /// <returns></returns>
+        public static double CalcMonthlyPayment(int investmentDuration, double investmentSum, double interestRate)
+        {
+            int monthsCount = CalcMonthsCount(investmentDuration);
+
+            return investmentSum / monthsCount + (investmentSum * (interestRate / 100)) / monthsCount;
+        }
+
+        /// <summary>
+        /// A function that calculates the number of months.
+        /// </summary>
+        /// <param name="investmentDuration"></param>
+        /// <returns></returns>
+        private static int CalcMonthsCount(int investmentDuration)  
+        {
+            return investmentDuration * 12;
+        }
+
+        /// <summary>
+        /// A function that calculates monthly payment from the beginning of the month to calculation date.
+        /// </summary>
+        /// <param name="calculationDate"></param>
+        /// <param name="investmentSum"></param>
+        /// <param name="interestRate"></param>
+        /// <returns></returns>
+        public static double CalcMonthlyPaymentToCalcDate(DateTime calculationDate, double investmentSum, double interestRate)
+        {
+            var firstDayInMonth = new DateTime(calculationDate.Year, calculationDate.Month, 1);
+            int daystoCalcDate = (calculationDate - firstDayInMonth).Days;
+
+            return investmentSum / daystoCalcDate + (investmentSum * (interestRate / 100)) / daystoCalcDate;
+        }
+
+        /// <summary>
+        /// A function that calculates sum of all future interest payments.
+        /// </summary>
+        /// <param name="investmentSum"></param>
+        /// <param name="interestRate"></param>
+        /// <returns></returns>
+        public static double CalcTotalSum(double investmentSum, double interestRate)
+        {
+            return investmentSum + (investmentSum * (interestRate / 100));
+        }
+
+        /// <summary>
+        /// A function that calculates sum of from agreement date to calculation date.
+        /// </summary>
+        /// <param name="calculationDate"></param>
+        /// <param name="agreementDate"></param>
+        /// <param name="investmentDuration"></param>
+        /// <param name="investmentSum"></param>
+        /// <param name="interestRate"></param>
+        /// <returns></returns>
+        public static double CalcTotalSumToCalcDate(DateTime calculationDate, DateTime agreementDate, int investmentDuration, double investmentSum, double interestRate)
+        {
+            int months = (calculationDate.Year - agreementDate.Year) * 12 + calculationDate.Month - agreementDate.Month;
+            double monthlyPayment = CalcMonthlyPayment(investmentDuration, investmentSum, interestRate);
+            double monthlyPaymentToCalcDate = CalcMonthlyPaymentToCalcDate(calculationDate, investmentSum, interestRate);
+
+            return monthlyPayment * months + monthlyPaymentToCalcDate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SimCorpTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an Agreement date (MM/dd/yyyy):");
+            var agreementDate = DateTime.Parse(Console.ReadLine()).Date;
+
+            Console.WriteLine("Enter a Calculation date (MM/dd/yyyy):");
+            var calculationDate = DateTime.Parse(Console.ReadLine()).Date;
+
+            Console.WriteLine("Enter an Investment sum (X $):");
+            var investmentSum = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an Interest rate (R %):");
+            var interestRate = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an Investment duration (N years):");
+            var investmentDuration = Convert.ToInt32(Console.ReadLine());
+
+            double monthlyPayment = InvestmentCalculation.CalcMonthlyPayment(investmentDuration, investmentSum, interestRate);
+            Console.WriteLine("Monthly payment: {0}", monthlyPayment);
+
+            double monthlyPaymentToCalcDate = InvestmentCalculation.CalcMonthlyPaymentToCalcDate(calculationDate, investmentSum, interestRate);
+            Console.WriteLine("Monthly payment to Calculation date: {0}", monthlyPaymentToCalcDate);
+
+            double totalSum = InvestmentCalculation.CalcTotalSum(investmentSum, interestRate);
+            Console.WriteLine("Total sum of all future interest payments: {0}", totalSum);
+
+            double totalSumCalcDate = InvestmentCalculation.CalcTotalSumToCalcDate(calculationDate, agreementDate, investmentDuration, investmentSum, interestRate);
+            Console.WriteLine("Total sum from Agreement date to Calculation date: {0}", totalSumCalcDate);
+
+            Console.ReadLine();
+        }  
+    }
+}
